b87f50da88dfbb6ad4a1c749fa2bcdfdfebfe6a1
pavxxx <pavithra020906@gmail.com>
2026-01-22 19:33:21 +0530

    intial commit , just the framework , tailwinds works  ahhahahh

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-green-500 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">
+        TAILWIND WORKS
+      </h1>
+    </div>
+  );
+}
